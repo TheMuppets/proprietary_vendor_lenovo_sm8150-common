03d623139b32ff5614302bdc7cd58b267b556ce8
Luofan Chen <clfbbn@gmail.com>
2024-10-01 06:29:01 +0000

    sm8150-common: Drop libOmxVideoDSMode
    * Not needed.
    
    Change-Id: I4da6b3fce55ec9fa953b097280740775dc6794fa

--- sm8150-common-vendor.mk
+++ sm8150-common-vendor.mk
@@ -239,7 +239,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vpp@1.1 \
     vendor.qti.hardware.vpp@1.2 \
     vendor.qti.hardware.vpp@1.3 \
-    libOmxVideoDSMode \
     libOmxVpp \
     libadpcmdec \
     libfastcrc \
